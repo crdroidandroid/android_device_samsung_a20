d1b0e45c170dcda16adfe05d557d229358f9fd97
Ivy <78825510+Lost-Entrepreneur439@users.noreply.github.com>
2024-10-01 15:05:12 -0230

    a20: move a20 files to root

--- universal7885-common/Android.mk
+++
@@ -1,8 +0,0 @@
-LOCAL_PATH := $(call my-dir)
-
-ifneq ($(filter a10dd a10 a20 a20e a30 a30s a40, $(TARGET_DEVICE)),)
-
-  subdir_makefiles=$(call first-makefiles-under,$(LOCAL_PATH))
-  $(foreach mk,$(subdir_makefiles),$(info including $(mk) ...)$(eval include $(mk)))
-
-endif
--- universal7885-common/vendorsetup.sh
+++
@@ -1,6 +0,0 @@
-if [ ! -e .repo/local_manifests/eureka_deps.xml ]; then
-	git clone https://github.com/eurekadevelopment/local_manifests .repo/local_manifests
-	echo "Run repo sync again"
-fi
-
-python3 device/samsung/universal7885-common/generate_product_makefiles.py
